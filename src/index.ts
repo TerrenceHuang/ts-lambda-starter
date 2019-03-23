4fc75f72b29b659b582645db198c8546767f6c28
TerrenceHuang <terrencehuang2000@gmail.com>
2019-03-23 11:29:47 +0800

    Lambda Entrypoint

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
-var hw: string = 'Hello World';
-
-console.log(hw);
+export const handler = async (event: any = {}): Promise<any> => {
+  console.log('Hello World!');
+  const response = JSON.stringify(event, null, 2);
+  return response;
+}
